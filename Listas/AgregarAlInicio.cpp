fe2280a277b9c28219916ac8fc2e1d32a4a9b329
Matias Fontes <matifontes13@gmail.com>
2020-10-02 01:36:27 -0300

    Create AgregarAlInicio.cpp

---
+++ Listas/AgregarAlInicio.cpp
@@ -0,0 +1,13 @@
+//PRE: Recibe una lista res, una lista aux y un nodo nuevo
+//POS: agrega nodo nuevo al principio de la lista res y en caso de que sea el primer nodo de res aux apunta a ese nodo.
+void agregarAlInicio(NodoLista* nuevo, NodoLista*& res, NodoLista*& aux) {
+	if (res == NULL) {
+		nuevo->sig = res;
+		res = nuevo;
+		aux = res;
+	}
+	else {
+		nuevo->sig = res;
+		res = nuevo;
+	}
+}
