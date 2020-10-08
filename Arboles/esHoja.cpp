8b212727902c71695040a3d595303fcbecff3d9c
Matias Fontes <matifontes13@gmail.com>
2020-10-08 20:16:20 -0300

    Create esHoja.cpp

---
+++ Arboles/esHoja.cpp
@@ -0,0 +1,30 @@
+bool esHoja(NodoAB* raiz) {
+	return raiz->izq == NULL && raiz->der == NULL;
+}
+
+bool existeCaminoConSuma(NodoAB* raiz, int sum) {
+	bool res = false;
+	if (raiz == NULL){
+		return true;
+	}
+	else {
+		int diff = sum - raiz->dato;
+		if (diff != 0 && esHoja(raiz)) {
+			return false;
+		}
+
+		if (diff == 0 && esHoja(raiz)) {
+			return true;
+		}
+
+		if (raiz->izq != NULL) {
+			res = existeCaminoConSuma(raiz->izq, diff);
+		}
+
+		if (raiz->der != NULL) {
+			res = res || existeCaminoConSuma(raiz->der, diff);
+		}
+
+		return res;
+	}
+}
