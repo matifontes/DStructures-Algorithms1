37de7997b914357c4bdbad05c08f58c543076c50
Matias Fontes <matifontes13@gmail.com>
2020-10-16 21:51:58 -0300

    Update AgregarAlInicio.cpp

--- Listas/AgregarAlInicio.cpp
+++ Listas/AgregarAlInicio.cpp
@@ -1,5 +1,6 @@
 //PRE: Recibe una lista res, una lista aux y un nodo nuevo
 //POS: agrega nodo nuevo al principio de la lista res y en caso de que sea el primer nodo de res aux apunta a ese nodo.
+
 void agregarAlInicio(NodoLista* nuevo, NodoLista*& res, NodoLista*& aux) {
 	if (res == NULL) {
 		nuevo->sig = res;
